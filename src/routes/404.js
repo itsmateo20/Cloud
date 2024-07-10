ad5ee328d1f93dcc7180310bb9a2d68606230b76
Mateusz Starkiewicz <itsmateo20@gmail.com>
2024-07-10 11:13:32 +0200

    refactor: Remove unused route files and theme HTML templates

--- src/routes/404.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    name: "404",
-    url: "/404",
-    run: async (req, res) => {
-        res.redirect('/')
-    }
-}
--- src/routes/register.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    name: "register",
-    url: "/register",
-    run: async (req, res) => {
-        res.redirect("/signup")
-    }
-}
